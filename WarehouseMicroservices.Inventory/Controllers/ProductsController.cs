895e058a0b90901006218d8b03ac13e64621d129
Jose Andres Hurtado <joseandreshurtadov@gmail.com>
2023-02-13 22:18:42 -0400

    Add Response DTO and Products Controller.

---
+++ WarehouseMicroservices.Inventory/Controllers/ProductsController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseMicroservices.Inventory.DTOs;
+using WarehouseMicroservices.Inventory.DTOs.Product;
+using WarehouseMicroservices.Inventory.Services.Interfaces;
+
+namespace WarehouseMicroservices.Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseDTO<IEnumerable<ProductDTO>>>> GetProducts()
+        {
+            var response = new ResponseDTO<IEnumerable<ProductDTO>>();
+            try
+            {
+                response.Result = await _productService.GetAllProducts();
+                response.Success = true;
+                response.Message = "Products obtained.";
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResponseDTO<ProductDTO>>> GetProduct(int id)
+        {
+            var response = new ResponseDTO<ProductDTO>();
+            try
+            {
+                response.Result = await _productService.GetProduct(id);
+                response.Success = true;
+                response.Message = "Product obtained.";
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ResponseDTO<ProductDTO>>> PutProduct(int id, UpdateProductDTO product)
+        {
+            var response = new ResponseDTO<ProductDTO>();
+            try
+            {
+                response.Result = await _productService.UpdateProduct(id, product);
+                response.Success = true;
+                response.Message = "Product updated.";
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseDTO<ProductDTO>>> PostProduct(CreateProductDTO product)
+        {
+            var response = new ResponseDTO<ProductDTO>();
+            try
+            {
+                response.Result = await _productService.CreateProduct(product);
+                response.Success = true;
+                response.Message = "Product created.";
+
+                return CreatedAtAction("GetProduct",
+                    new { id = response.Result.Id }, response);
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+
+                return Ok(response);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var response = new ResponseDTO<int>();
+            try
+            {
+                await _productService.DeleteProduct(id);
+
+                response.Result = id;
+                response.Success = true;
+                response.Message = "Product deleted.";
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ WarehouseMicroservices.Inventory/DTOs/ResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseMicroservices.Inventory.DTOs
+{
+    public class ResponseDTO<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Result { get; set; }
+    }
+}
